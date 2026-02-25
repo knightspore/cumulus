12d787b19b90606fce1ef0fd929fc59f43204160
Ciaran <cslemon96@gmail.com>
2026-02-25 18:03:05 +0200

    refactor(resolution.test.ts): use real tid

--- packages/lexicon/src/resolution.test.ts
+++ packages/lexicon/src/resolution.test.ts
@@ -2,14 +2,16 @@ import { parse } from "@atcute/lexicons";
 import { it, expect } from "vitest";
 import { ZaCoCiaranCumulusResolution } from "../generated/typescript";
 import * as CID from "@atcute/cid";
+import * as TID from "@atcute/tid";
 
+const tid = TID.now();
 const cid = await CID.create(0x71, new Uint8Array([10]))
 
 const data = {
     $type: "za.co.ciaran.cumulus.resolution",
     market: {
         cid: CID.toString(cid),
-        uri: "at://did:plc:example/za.co.ciaran.cumulus.market/tid"
+        uri: `at://did:plc:example/za.co.ciaran.cumulus.market/${tid}`
     },
     answer: "yes",
     createdAt: "2026-02-25T11:52:33.278Z",
@@ -20,7 +22,7 @@ it("market schema validation", () => {
     expect(resolution.$type).toEqual("za.co.ciaran.cumulus.resolution");
     expect(resolution.market).toEqual({
         cid: CID.toString(cid),
-        uri: "at://did:plc:example/za.co.ciaran.cumulus.market/tid"
+        uri: `at://did:plc:example/za.co.ciaran.cumulus.market/${tid}`
     });
     expect(resolution.answer).toEqual("yes");
     expect(resolution.createdAt).toEqual("2026-02-25T11:52:33.278Z");
