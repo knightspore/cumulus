3d6496956ada7b72577ad8e8ee7f5471adcaf7e6
Ciaran <cslemon96@gmail.com>
2026-02-27 22:32:41 +0200

    refactor(packages/jetstream-store): move to setup db

--- packages/jetstream-store/index.ts
+++ packages/jetstream-store/index.ts
@@ -1,25 +1,22 @@
 import { JetstreamSubscription } from "@atcute/jetstream"
 
 const URL = "wss://jetstream2.us-east.bsky.network";
-
-const COLLECTIONS = [
-    'za.co.ciaran.cumulus.bet',
-    'za.co.ciaran.cumulus.market',
-    'za.co.ciaran.cumulus.resolution',
-]
-
-console.log(`> Connecting to ${URL} and listening for events from ${COLLECTIONS}`);
+const COLLECTIONS = ['za.co.ciaran.cumulus.bet', 'za.co.ciaran.cumulus.market', 'za.co.ciaran.cumulus.resolution']
+const cursor = Date.now() * 1_000 - (36 * 60 * 60 * 1_000_000); // 36h in microseconds;
 
 const subscription = new JetstreamSubscription({
     url: URL,
     wantedCollections: COLLECTIONS,
+    cursor,
     onConnectionOpen: () => console.warn("> Connected to Jetstream: ", URL),
     onConnectionClose: () => console.warn("> Disconnected from Jetstream: ", URL),
     onConnectionError: (e) => console.error("> Error in Jetstream Subscription: ", e),
 });
 
+console.log(`> Connecting to ${URL} and listening for events from ${COLLECTIONS}`);
+
 for await (const event of subscription) {
     if (event.kind === "commit") {
-        console.log(event.commit);
+        console.log(event.kind, event.commit);
     }
 }
