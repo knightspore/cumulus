9727a504fa01c85f94ec48fcac2528cbe48a904c
Ciaran <cslemon96@gmail.com>
2026-03-01 00:55:21 +0200

    feat(jetstream/index.ts): improve collection filter, remove cursor

--- src/jetstream/index.ts
+++ src/jetstream/index.ts
@@ -1,14 +1,11 @@
 import { JetstreamSubscription } from "@atcute/jetstream"
-import { $ } from "bun"
 
 const URL = "wss://jetstream2.us-east.bsky.network";
-const COLLECTIONS = ['za.co.ciaran.cumulus.bet', 'za.co.ciaran.cumulus.market', 'za.co.ciaran.cumulus.resolution']
-const cursor = Date.now() * 1_000 - (36 * 60 * 60 * 1_000_000); // 36h in microseconds;
+const COLLECTIONS = ['za.co.ciaran.cumulus.*']
 
 const subscription = new JetstreamSubscription({
     url: URL,
     wantedCollections: COLLECTIONS,
-    cursor,
     onConnectionOpen: () => console.warn("> Connected to Jetstream: ", subscription.getOptions().url),
     onConnectionClose: () => console.warn("> Disconnected from Jetstream: ", subscription.getOptions().url),
     onConnectionError: (e) => console.error("> Error in Jetstream Subscription: ", e),
