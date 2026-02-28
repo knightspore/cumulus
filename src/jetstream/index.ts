bba28ccac581cf159c60974ed9664ab82aa15827
Ciaran <cslemon96@gmail.com>
2026-02-28 20:37:51 +0200

    chore(src/jetstream): cleanup

--- src/jetstream/index.ts
+++ src/jetstream/index.ts
@@ -1,4 +1,5 @@
 import { JetstreamSubscription } from "@atcute/jetstream"
+import { $ } from "bun"
 
 const URL = "wss://jetstream2.us-east.bsky.network";
 const COLLECTIONS = ['za.co.ciaran.cumulus.bet', 'za.co.ciaran.cumulus.market', 'za.co.ciaran.cumulus.resolution']
@@ -8,12 +9,12 @@ const subscription = new JetstreamSubscription({
     url: URL,
     wantedCollections: COLLECTIONS,
     cursor,
-    onConnectionOpen: () => console.warn("> Connected to Jetstream: ", URL),
-    onConnectionClose: () => console.warn("> Disconnected from Jetstream: ", URL),
+    onConnectionOpen: () => console.warn("> Connected to Jetstream: ", subscription.getOptions().url),
+    onConnectionClose: () => console.warn("> Disconnected from Jetstream: ", subscription.getOptions().url),
     onConnectionError: (e) => console.error("> Error in Jetstream Subscription: ", e),
 });
 
-console.log(`> Connecting to ${URL} and listening for events from ${COLLECTIONS}`);
+console.log(`> Connecting to ${subscription.getOptions().url} and listening for events from ${subscription.getOptions().wantedCollections?.join(', ')}`);
 
 for await (const event of subscription) {
     if (event.kind === "commit") {
