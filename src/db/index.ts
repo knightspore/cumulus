9e341133eb45f11ea326dc033b95b9422ed070cc
Ciaran <cslemon96@gmail.com>
2026-03-04 17:44:08 +0200

    feat(src/db/index.ts): add functions to create bets and resolutions

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import { drizzle } from "drizzle-orm/node-postgres";
-import { ZaCoCiaranCumulusMarket } from '../../generated/typescript';
-import { marketsTable } from './schema';
+import { ZaCoCiaranCumulusBet, ZaCoCiaranCumulusMarket, ZaCoCiaranCumulusResolution } from '../../generated/typescript';
 import { is, type ActorIdentifier } from '@atcute/lexicons';
 import type { CreateCommit } from '@atcute/jetstream';
 import * as schema from "./schema"
@@ -10,24 +9,51 @@ export const db = drizzle(process.env.DATABASE_URL!, { schema });
 
 export async function tryCreateMarket(did: ActorIdentifier, { record, rev, rkey, cid }: CreateCommit) {
     if (is(ZaCoCiaranCumulusMarket.mainSchema, record)) {
-        console.log("> Creating Market: ", rkey);
-        const marketData: typeof marketsTable.$inferInsert = {
-            uri: `at://${did}/${record.$type}/${rkey}`,
-            did,
-            rev,
-            rkey,
-            cid,
-            question: record.question,
-            liquidity: record.liquidity,
-            closesAt: new Date(record.closesAt),
-            record,
-            createdAt: new Date(record.createdAt),
+        console.log("> Creating Market:", rkey);
+
+        const uri = `at://${did}/${record.$type}/${rkey}`;
+        const { question, liquidity } = record;
+        const [closesAt, createdAt] = [new Date(record.closesAt), new Date(record.createdAt)];
+
+        const marketData: typeof schema.marketsTable.$inferInsert = {
+            uri, did, rev, rkey, cid, question, liquidity, closesAt, record, createdAt,
         };
 
-        await db.insert(marketsTable).values(marketData);
+        await db.insert(schema.marketsTable).values(marketData);
         console.log("> Created Market!");
+    }
+}
 
-        const markets = await db.select().from(marketsTable);
-        console.log("Getting all markets from the database: ", markets);
+export async function tryCreateBet(did: ActorIdentifier, { record, rev, rkey, cid }: CreateCommit) {
+    if (is(ZaCoCiaranCumulusBet.mainSchema, record)) {
+        console.log("> Creating Bet:", rkey);
+
+        const uri = `at://${did}/${record.$type}/${rkey}`;
+        const { position, market } = record;
+        const createdAt = new Date(record.createdAt);
+
+        const betData: typeof schema.betsTable.$inferInsert = {
+            uri, did, rev, rkey, cid, position, marketUri: market.uri, record, createdAt
+        }
+
+        await db.insert(schema.betsTable).values(betData);
+        console.log("> Created Bet!");
+    }
+}
+
+export async function tryCreateResolution(did: ActorIdentifier, { record, rev, rkey, cid }: CreateCommit) {
+    if (is(ZaCoCiaranCumulusResolution.mainSchema, record)) {
+        console.log("> Creating Resolution:", rkey);
+
+        const uri = `at://${did}/${record.$type}/${rkey}`;
+        const { answer, market } = record;
+        const createdAt = new Date(record.createdAt);
+
+        const resolutionData: typeof schema.resolutionsTable.$inferInsert = {
+            uri, did, rev, rkey, cid, answer, marketUri: market.uri, record, createdAt
+        }
+
+        await db.insert(schema.resolutionsTable).values(resolutionData);
+        console.log("> Created Resolution!");
     }
 }
--- src/jetstream/index.ts
+++ src/jetstream/index.ts
@@ -1,27 +1,30 @@
 import { type CreateCommit, type DeleteCommit, type UpdateCommit } from "@atcute/jetstream"
 import { jetstream } from "./config";
-import { tryCreateMarket } from "../db/index.ts";
+import { tryCreateBet, tryCreateMarket, tryCreateResolution } from "../db/index.ts";
 import type { ActorIdentifier } from "@atcute/lexicons";
 
 console.log(`> Connecting to ${jetstream.getOptions().url} and listening for events from ${jetstream.getOptions().wantedCollections?.join(', ')}`);
 
 async function handleCreate(did: ActorIdentifier, commit: CreateCommit) {
+    console.log("Handling Create Commit:", commit.rkey);
     tryCreateMarket(did, commit);
+    tryCreateBet(did, commit);
+    tryCreateResolution(did, commit);
 }
 
-async function handleUpdate(commit: UpdateCommit) {
-    console.log("update", commit);
+async function handleUpdate(did: ActorIdentifier, commit: UpdateCommit) {
+    console.log("Handling Update Commit:", commit.rkey);
 }
 
-async function handleDelete(commit: DeleteCommit) {
-    console.log("delete", commit);
+async function handleDelete(did: ActorIdentifier, commit: DeleteCommit) {
+    console.log("Handling Delete Commit:", commit.rkey);
 }
 
 for await (const event of jetstream) {
     if (event.kind === "commit") switch (event.commit.operation) {
         case "create": handleCreate(event.did, event.commit); break;
-        case "update": handleUpdate(event.commit); break;
-        case "delete": handleDelete(event.commit); break;
+        case "update": handleUpdate(event.did, event.commit); break;
+        case "delete": handleDelete(event.did, event.commit); break;
         default: throw new Error("Unknown Commit Type: " + event);
     }
 }
