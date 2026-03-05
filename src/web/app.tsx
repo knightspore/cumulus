a836b6a22f80a6e0a1aaeaf889dbd72e11e2705d
Ciaran <cslemon96@gmail.com>
2026-03-05 15:57:19 +0200

    chore(app.tsx): cleanup

--- src/web/app.tsx
+++ src/web/app.tsx
@@ -1,27 +1,29 @@
 import { useCumulus } from "./providers/useCumulus";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
 import { formatDistance } from "date-fns"
-import type { Market } from "./providers/cumulus-provider";
 
 export default function App() {
-    const { markets } = useCumulus();
     return <div className="p-4 space-y-4">
         <h2>Markets</h2>
-        {markets.data?.map(m => <Market key={m.cid} market={m} />)}
+        <Markets />
     </div>
 }
 
-function Market({ market }: { market: Market }) {
-    return <Card>
-        <CardHeader>
-            <CardTitle>{market.question}</CardTitle>
-            <CardDescription>Closes {formatDistance(new Date(market.closesAt), new Date(), { addSuffix: true })}</CardDescription>
-        </CardHeader>
-        <CardContent>
-            <p>Resolution: {market.resolution?.answer}</p>
-        </CardContent>
-        <CardFooter>
-            <p>Bets: {market.bets?.length}</p>
-        </CardFooter>
-    </Card>
+function Markets() {
+    const { markets } = useCumulus();
+    return <>
+        {markets.data?.map(market => (<Card>
+            <CardHeader>
+                <CardTitle>{market.question}</CardTitle>
+                <CardDescription>Closes {formatDistance(new Date(market.closesAt), new Date(), { addSuffix: true })}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>Resolution: {market.resolution?.answer}</p>
+            </CardContent>
+            <CardFooter>
+                <p>Bets: {market.bets?.length}</p>
+            </CardFooter>
+        </Card>
+        ))}
+    </>
 }
