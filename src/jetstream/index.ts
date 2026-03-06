58c3ed00cc75038f0382a37b929bdf67ae70dd40
Ciaran <cslemon96@gmail.com>
2026-03-06 15:00:56 +0200

    feat(jetstream/index.ts): move config here

--- src/jetstream/index.ts
+++ src/jetstream/index.ts
@@ -1,7 +1,18 @@
 import { type CreateCommit, type DeleteCommit, type UpdateCommit } from "@atcute/jetstream"
-import { jetstream } from "./config";
 import { tryCreateBet, tryCreateMarket, tryCreateResolution, tryDeleteBet, tryDeleteMarket, tryDeleteResolution } from "@/core/api"
 import type { ActorIdentifier } from "@atcute/lexicons";
+import { JetstreamSubscription } from "@atcute/jetstream"
+
+const URL = "wss://jetstream2.us-east.bsky.network";
+const COLLECTIONS = ['za.co.ciaran.cumulus.*']
+
+export const jetstream = new JetstreamSubscription({
+    url: URL,
+    wantedCollections: COLLECTIONS,
+    onConnectionOpen: () => console.log("> Connected to Jetstream: ", jetstream.getOptions().url),
+    onConnectionClose: () => console.log("> Disconnected from Jetstream: ", jetstream.getOptions().url),
+    onConnectionError: (e) => console.error("> Error in Jetstream Subscription: ", e),
+});
 
 console.log(`> Connecting to ${jetstream.getOptions().url} and listening for events from ${jetstream.getOptions().wantedCollections?.join(', ')}`);
 
